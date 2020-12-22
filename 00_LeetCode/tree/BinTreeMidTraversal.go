3465ac8cd014f3de9439a7f2d194738928c6d3f0
pixeldin <626995617@qq.com>
2020-12-23 00:57:16 +0800

    tree traversal

---
+++ 00_LeetCode/tree/BinTreeMidTraversal.go
@@ -0,0 +1,57 @@
+package tree
+
+import "fmt"
+
+/**
+* Definition for a binary tree node.
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func TestMinTravel() {
+	tr := buildTree()
+	traversal := inorderTraversal(tr)
+	fmt.Println(traversal)
+}
+
+func inorderTraversal(root *TreeNode) []int {
+	ret := make([]int, 0)
+	midTraversal(root, &ret)
+	return ret
+}
+
+func midTraversal(root *TreeNode, ret *[]int) {
+	if root == nil {
+		return
+	}
+	if root.Left != nil {
+		midTraversal(root.Left, ret)
+	}
+	//midTraversal(root, ret)
+	*ret = append(*ret, root.Val)
+	if root.Right != nil {
+		midTraversal(root.Right, ret)
+	}
+}
+
+func buildTree() *TreeNode {
+	left := TreeNode{
+		3,
+		nil,
+		nil,
+	}
+	right := TreeNode{
+		2,
+		&left,
+		nil,
+	}
+	root := TreeNode{
+		1,
+		nil,
+		&right,
+	}
+	return &root
+}
