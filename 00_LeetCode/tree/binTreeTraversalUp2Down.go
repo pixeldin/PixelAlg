56cf460fa00e1f75237574952371a489033dacf8
pixeldin <626995617@qq.com>
2021-04-13 00:12:55 +0800

    traversal tree with lvl

--- 00_LeetCode/tree/binTreeTraversalUp2Down.go
+++ 00_LeetCode/tree/binTreeTraversalUp2Down.go
@@ -7,26 +7,34 @@ package tree
   思路2: DFS, 深度优先遍历左子树, 每进入一层层数+1, 每层存储一个单独的切片.
 */
 func levelOrder(root *TreeNode) [][]int {
-	ret := make([][]int, 0)
-	if root != nil {
-		ret[0] = append(ret[0], root.Val)
+	if root == nil {
+		return [][]int{}
 	}
+	ret := make([][]int, 0)
+	lv := make([]int, 0)
+	lv = append(lv, root.Val)
+	ret = append(ret, lv)
 	if root.Left != nil {
-		markTreeLvl(root.Left, ret, 1)
+		markTreeLvl(root.Left, &ret, 1)
 	}
 	if root.Right != nil {
-		markTreeLvl(root.Right, ret, 1)
+		markTreeLvl(root.Right, &ret, 1)
 	}
-	return nil
+	return ret
 }
 
-func markTreeLvl(node *TreeNode, ret [][]int, lvl int) {
-	ret[lvl] = append(ret[lvl], node.Val)
+func markTreeLvl(node *TreeNode, ret *[][]int, lvl int) {
+	//fmt.Println(node.Val)
+	if len(*ret) < lvl+1 {
+		lv := make([]int, 0)
+		*ret = append(*ret, lv)
+	}
+	(*ret)[lvl] = append((*ret)[lvl], node.Val)
 	lvl++
 	if node.Left != nil {
 		markTreeLvl(node.Left, ret, lvl)
 	}
 	if node.Right != nil {
-		markTreeLvl(node.Right, ret, 1)
+		markTreeLvl(node.Right, ret, lvl)
 	}
 }
--- 00_LeetCode/tree/tree_test.go
+++ 00_LeetCode/tree/tree_test.go
@@ -2,6 +2,27 @@ package tree
 
 import "testing"
 
+func buildLocalTree() *TreeNode {
+	a := BuildTreeWithThree(1, 2, 3)
+	b := BuildTreeWithThree(2, 4, 5)
+	c := BuildTreeWithThree(3, 6, 7)
+	d := BuildTreeWithThree(4, 8, 9)
+	e := BuildTreeWithThree(5, 10, 11)
+	f := BuildTreeWithThree(6, 12, 13)
+	g := BuildTreeWithThree(7, 12, 13)
+	a.Left = b
+	a.Right = c
+
+	b.Left = d
+	b.Right = e
+
+	c.Left = f
+	c.Right = g
+
+	return a
+}
+
 func TestLevelOrder(t *testing.T) {
-	levelOrder()
+	tree := buildLocalTree()
+	levelOrder(tree)
 }
